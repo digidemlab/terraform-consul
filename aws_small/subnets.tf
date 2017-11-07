f79bb9d7df149a2aa60c4c5a025ba0f4aa442813
munjeli <elemunjeli@gmail.com>
2017-11-07 16:27:03 +0100

    state, backups, and plugins init

---
+++ aws_small/subnets.tf
@@ -0,0 +1,38 @@
+resource "aws_subnet" "service" {
+  vpc_id                  = "${aws_vpc.small.id}"
+  availability_zone       = "${var.consul_deploy_region}a"
+  cidr_block              = "10.100.3.0/24"
+  map_public_ip_on_launch = "false"
+  tags {
+    Name = "service"
+  }
+}
+
+resource "aws_subnet" "data" {
+  vpc_id                  = "${aws_vpc.small.id}"
+  availability_zone       = "${var.consul_deploy_region}b"
+  cidr_block              = "10.100.4.0/24"
+  map_public_ip_on_launch = "false"
+  tags {
+    Name = "data"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = "${aws_vpc.small.id}"
+  availability_zone       = "${var.consul_deploy_region}a"
+  cidr_block              = "10.100.5.0/24"
+  map_public_ip_on_launch = "true"
+  tags {
+    Name = "public"
+  }
+}
+
+resource "aws_db_subnet_group" "small-data" {
+    name = "small-data"
+    description = "subgroup for rds"
+    subnet_ids = ["${aws_subnet.data.id}", "${aws_subnet.service.id}"]
+    tags {
+        Name = "data-group"
+    }
+}
